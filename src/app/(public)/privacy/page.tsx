77744044c77587703a8047523e88d6b4fd7212ca
nicoladimaio <49908808+nicoladimaio@users.noreply.github.com>
2025-12-29 11:40:48 +0100

    Modifica per pagina PRIVACY

--- src/app/(public)/privacy/page.tsx
+++ src/app/(public)/privacy/page.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import PreventivoFooter from "../components/PreventivoFooter";
+import PreventivoFooter from "../../components/PreventivoFooter";
 
 const sections = [
   {
